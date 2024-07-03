79eae5d6440b78b9420c67b1fca536300310edb8
mraihanfauzii <muhammad.raihan.fauzi@gmail.com>
2024-07-04 00:41:08 +0700

    Initial Commit

---
+++ app/src/main/java/com/hackathon/devlabsuser/adapter/architect/PortfolioAdapter.kt
@@ -0,0 +1,4 @@
+package com.hackathon.devlabsuser.adapter.architect
+
+class PortfolioAdapter {
+}
---
+++ app/src/main/java/com/hackathon/devlabsuser/api/ApiConfig.kt
@@ -0,0 +1,28 @@
+package com.hackathon.devlabsuser.api
+
+import com.hackathon.devlabsuser.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiConfig {
+    private const val baseUrl = ""
+
+    fun getABCApiService(): ApiService {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+}
